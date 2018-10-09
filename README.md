8dc0f68746a89c82eb31973ab122ee635db383e2
wanls4583 <409051987@qq.com>
2018-10-09 23:36:36 +0800

    lcs

---
+++ README.md
@@ -0,0 +1,28 @@
+# js-dp-lcs
+
+> 使用动态规划求最长连续的公共子串（数组或字符串）
+
+## 内容
+
+- [**`案例`**](#案例)
+- [**`getLCS`**](#getLCS)
+- [**`贡献`**](#贡献)
+
+## 案例
+
+请查看[**`example`**](https://github.com/wanls4583/js-dp-lcs/tree/master/src/example)
+
+## getLCS
+
+```javascript
+/**
+ * @param  {Array/String} 数组1
+ * @param  {Array/String} 数组1
+ * @return {Array/String} 最大连续的公共子串
+ */
+function getLCS(arr1, arr2)
+```
+
+## 贡献
+
+欢迎给出一些意见和优化，期待你的 `Pull Request`
---
+++ src/lcs.js
@@ -0,0 +1,33 @@
+/**
+ * @param  {Array/String} 数组1
+ * @param  {Array/String} 数组1
+ * @return {Array/String} 最大连续的公共子串
+ */
+function getLCS(arr1, arr2) {
+    var sub = arr1 instanceof Array ? [] : '';
+    if (!arr1.length || !arr2.length) {
+        return sub;
+    }
+    var maxSize = 0;
+    var preSize = 0;
+    var index = -1;
+    for (var i = 0; i < arr1.length; i++) {
+        var preIndex = i;
+        for (var j = 0; j < arr2.length; j++) {
+            if (arr1[preIndex] == arr2[j]) {
+                preIndex++;
+                preSize++;
+                if (preSize > maxSize) {
+                    maxSize = preSize;
+                    index = j;
+                }
+            } else {
+                preSize = 0;
+            }
+        }
+    }
+    if (index > -1) {
+        sub = arr2.slice(index - maxSize + 1, index + 1);
+    }
+    return sub;
+}
